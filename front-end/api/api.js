206eed5796b313e39a7c890825cf673442040774
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2025-02-14 15:28:20 -0300

    Chore: Update npm build remove dotenv

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,5 @@
-import "dotenv/config"
 import axios from "axios";
 
-//const { NODE_ENV } = process.env;
-//const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
 const URL = "https://fullstack-journey-intensive-class.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
