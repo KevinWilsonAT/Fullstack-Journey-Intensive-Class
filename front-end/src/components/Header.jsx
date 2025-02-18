387f5ac5a1b477e8dff76724ab7331b17bf04986
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2025-02-18 13:24:34 -0300

    Chore: Update npm build, Update project name, Update icon

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import logoSpotify from '../assets/logo/spotify-logo.png';
+import logoSoundify from '../assets/logo/soundify-logo.png';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className='header'>
         <Link to="/">
-          <img src={ logoSpotify } alt="Logo Spotify" />
+          <img src={ logoSoundify } alt="Logo Soundify" />
         </Link>
 
         <Link to="/" className='header__link'>
-            <h1>Spotify</h1>
+            <h1>Soundify</h1>
         </Link>
     </div>
   );
